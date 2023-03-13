2b07aafc14835ba62b6467f44d37fb19cf8d1fb4
Ghayoor <ghayoorhussain701@gmail.com>
2023-03-13 06:22:58 +0500

    Variables Expressions Convertions Done

---
+++ basic/expressions_and_variables.py
@@ -0,0 +1,10 @@
+print(type("a"))
+print(type(1))
+print(type(10.0))
+
+# ?Variable
+length = 10
+width = 20
+#!Expressions Are numbers symbols or other var thats produce result when evaluated
+area = length * width
+print(area)
---
+++ basic/implicit_explicit_convertions.py
@@ -0,0 +1,16 @@
+# implicit convertion --> interprater automatically convert
+print(7 + 8.5)
+
+print("This " + "is " + " pretty " + " Clean!")
+
+# ? explicit conversion is where we manually convert from one data type to another by calling the relevant function for the data type we want to convert to
+base = 6
+height = 8
+area = (base * height) / 2
+
+print("The Area of Triangle is " + str(area))
+
+total = 2048 + 4357 + 97658 + 125 + 8
+files = 5
+average = total / files
+print("Average: " + str(average))
