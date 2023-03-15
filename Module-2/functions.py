feff24c8797f9df307c76be64fd39c2a415debf8
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-03-15 05:18:09 +0500

    Functions

--- Module-2/functions.py
+++ Module-2/functions.py
@@ -5,3 +5,33 @@ def greeting(name, department):
 
 greeting("Ghayoor Hussain", "IT Support")
 
+
+# ? Return statement func
+def area_triangle(base, height):
+    return base * height / 2
+
+
+area_a = area_triangle(5, 7)
+area_b = area_triangle(7, 3)
+
+sum = area_a + area_b
+print("The sum of both areas is " + str(sum))
+
+
+def convert_second(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remain_sec = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remain_sec
+
+
+hours, minutes, seconds = convert_second(5000)
+print(hours, minutes, seconds)
+
+
+def lucky_number(name):
+    number = len(name) * 9
+    print("Hello " + name + ". Your lucky number is " + str(number))
+
+
+lucky_number("Ghayoor")
